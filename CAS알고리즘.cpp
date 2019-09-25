b3f9eb67defee84f57d2802781152357677882fb
SnowFleur <SnowFleur0128@gmail.com>
2019-09-26 00:12:33 +0900

    ADD 19.09.26

--- CAS알고리즘.cpp
+++ CAS알고리즘.cpp
@@ -1,18 +1,10 @@
+
 #include <iostream>
 #include <thread>
 #include <vector>
 #include<mutex>
 #include <chrono>
-#include<Windows.h>
-
-
-#ifdef WIN32
-BOOL CAS(LONG volatile* Addr, LONG Old, LONG New) {
-	LONG temp = InterlockedCompareExchange(Addr, New, Old);
-	return temp == Old;
-}
-#endif
-
+#include<atomic>
 
 using namespace std::chrono;
 constexpr int MAX_NUMBER = 100000000;
@@ -21,4 +13,56 @@ volatile int sum;
 std::mutex lock;
 
 
-int main() { system("pause"); }
+
+int x;  // 0-> Lock이 Free다.
+		// 1-> 누군가 Lock을 얻어서 CS를 실행중
+bool CAS(int* addr, int exp, int up) {
+	return atomic_compare_exchange_strong(reinterpret_cast<std::atomic_int*>(addr), &exp, up);
+}
+
+void CASLock() { while (!CAS(&x, 0, 1)); }
+void CASUnlock() { CAS(&x, 1, 0); }
+
+void AddFunc(int num_threads, int myid) {
+	for (int i = 0; i < MAX_NUMBER / num_threads; ++i) {
+		CASLock();
+		sum += 2;
+		CASUnlock();
+	}
+}
+
+int main() {
+	for (int num_thread = 1; num_thread <= MAX_THREAD; num_thread *= 2) {
+		sum = 0;
+		g_bakery.InitFlag(num_thread);
+		g_bakery.InitTicketNumber(num_thread);
+		std::vector<std::thread> threads;
+
+		auto start_t = high_resolution_clock::now();
+		for (int i = 0; i < num_thread; ++i) threads.emplace_back(AddFunc, num_thread, i);
+
+		for (auto &th : threads) th.join();
+
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+
+		threads.clear();
+		int ex_ms = duration_cast<milliseconds>(exec_t).count();
+
+		std::cout << "Threads [ " << num_thread << " ] Sum = " << sum;
+		std::cout << ", Exec Time = " << ex_ms << std::endl;
+		g_bakery.DeleteFlag();
+		g_bakery.DeleteTicketNumber();
+	}
+
+	system("pause");
+}
+
+
+
+
+
+
+
+
+
--- 빵집 알고리즘.cpp
+++ 빵집 알고리즘.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include<mutex>
 #include <chrono>
+#include<atomic>
 
 using namespace std::chrono;
 constexpr int MAX_NUMBER = 100000000;
@@ -28,48 +29,11 @@ public:
 
 Bakery g_bakery;
 
-void AddFunc(int num_threads, int myid) {
-	for (int i = 0; i < MAX_NUMBER / num_threads; ++i) {
-		//lock.lock();
-		g_bakery.Lock(num_threads, myid);
-		sum++;
-		g_bakery.UnLock(myid);
-		//lock.unlock();
-	}
-}
-
-int main() {
-	for (int num_thread = 1; num_thread <= MAX_THREAD; num_thread *= 2) {
-		sum = 0;
-		g_bakery.InitFlag(num_thread);
-		g_bakery.InitTicketNumber(num_thread);
-		std::vector<std::thread> threads;
-
-		auto start_t =high_resolution_clock::now();
-		for (int i = 0; i < num_thread; ++i) threads.emplace_back(AddFunc, num_thread, i);
-
-		for (auto &th : threads) th.join();
-
-		auto end_t = high_resolution_clock::now();
-		auto exec_t = end_t - start_t;
-
-		threads.clear();
-		int ex_ms = duration_cast<milliseconds>(exec_t).count();
-
-		std::cout << "Threads [ " << num_thread << " ] Sum = " << sum;
-		std::cout << ", Exec Time = " << ex_ms << std::endl;
-		g_bakery.DeleteFlag();
-		g_bakery.DeleteTicketNumber();
-	}
-
-	system("pause");
-}
-
 
 void Bakery::InitFlag(const int number) {
 	if (m_flag == nullptr) {
 		m_flag = new bool[number];
-		for (int i = 0; i < number; ++i) {m_flag[i] = false;}
+		for (int i = 0; i < number; ++i) { m_flag[i] = false; }
 	}
 	else
 		std::cout << "Error:InitFlag() flag is not nullptr" << std::endl;
@@ -101,21 +65,18 @@ void Bakery::DeleteTicketNumber() {
 
 }
 
-
-
-void Bakery::Lock(const int num_thread,const int thread_id){
+void Bakery::Lock(const int num_thread, const int thread_id) {
 
 	m_flag[thread_id] = true; //들어온 Thread 의 Flag를 킨다.
 	m_ticketNumber[thread_id] = GetTickNumber(num_thread); //번호표 부여
 	m_flag[thread_id] = false;
 	for (int i = 0; i < num_thread; ++i) {
 		while (m_flag[i]); //내 flag 넘버가 true가 아니면 무한대기한다.
-	
+
 		//번호표를 가지고 있고 번호표가 더 크다면 대기
 		while (m_ticketNumber[i] && m_ticketNumber[i] < m_ticketNumber[thread_id]);
 	}
 }
-
 int Bakery::GetTickNumber(const int number_thread) {
 	int max = 0;
 	for (int i = 0; i < number_thread; ++i) {
@@ -123,13 +84,11 @@ int Bakery::GetTickNumber(const int number_thread) {
 	}
 	return max + 1;
 }
-
-void Bakery::UnLock(const int thread_id) { 
+void Bakery::UnLock(const int thread_id) {
 	m_flag[thread_id] = false;
 	m_ticketNumber[thread_id] = 0;
 }
-
-Bakery::~Bakery() { 
+Bakery::~Bakery() {
 	if (m_flag != nullptr) {
 		delete[] m_flag;
 		m_flag = nullptr;
@@ -139,6 +98,40 @@ Bakery::~Bakery() {
 }
 
 
+void AddFunc(int num_threads, int myid) {
+	for (int i = 0; i < MAX_NUMBER / num_threads; ++i) {
+		g_bakery.Lock(num_threads, myid);
+		sum+=2;
+		g_bakery.UnLock(myid);
+	}
+}
+
+int main() {
+	for (int num_thread = 1; num_thread <= MAX_THREAD; num_thread *= 2) {
+		sum = 0;
+		g_bakery.InitFlag(num_thread);
+		g_bakery.InitTicketNumber(num_thread);
+		std::vector<std::thread> threads;
+
+		auto start_t =high_resolution_clock::now();
+		for (int i = 0; i < num_thread; ++i) threads.emplace_back(AddFunc, num_thread, i);
+
+		for (auto &th : threads) th.join();
+
+		auto end_t = high_resolution_clock::now();
+		auto exec_t = end_t - start_t;
+
+		threads.clear();
+		int ex_ms = duration_cast<milliseconds>(exec_t).count();
+
+		std::cout << "Threads [ " << num_thread << " ] Sum = " << sum;
+		std::cout << ", Exec Time = " << ex_ms << std::endl;
+		g_bakery.DeleteFlag();
+		g_bakery.DeleteTicketNumber();
+	}
+
+	system("pause");
+}
 
 
 
