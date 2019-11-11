d76f429ee64db8199d0373f8b8cbc37461d92667
SnowFleur <SnowFleur0128@gmail.com>
2019-11-12 01:39:08 +0900

    ADD Lock-free Queue

--- Queue/Lock-FreeQueue.cpp
+++ Queue/Lock-FreeQueue.cpp
@@ -1,135 +1,179 @@
-#include<iostream>
-#include<thread>
-#include<mutex>
-#include<chrono>
-#include<windows.h>
-#include<vector>
-#include<atomic>
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <vector>
 
+using namespace std;
 using namespace std::chrono;
-const auto NUM_TEXT = 10000000;
+const auto NUM_TEST = 10000000;
 const auto KEY_RANGE = 1000;
-
 
 
 class NODE {
 public:
 	int key;
 	NODE* next;
+
 	NODE() { next = nullptr; }
 	NODE(int key_value) {
 		next = nullptr;
 		key = key_value;
 	}
+	~NODE() {}
 };
 
-class Cqueue {
-	NODE* volatile head;
-	NODE* volatile tail;
-	std::mutex glock;
+//Steamp pointer
+class SPTR {
 public:
-
-	Cqueue() { head = tail = new NODE(0); }
-
-	bool CAS(NODE* tail, NODE* last, NODE* next) {
-
+	NODE* volatile ptr;
+	volatile int stamp;
+	SPTR() {
+		ptr = nullptr;
+		stamp = 0;
 	}
-	bool CAS(int old_v, int new_v) {
-		return std::atomic_compare_exchange_strong(reinterpret_cast<std::atomic_int*>(this), &old_v, new_v);
+	SPTR(NODE* p, int v) {
+		ptr = p;
+		stamp = v;
 	}
+};
 
+class SLFQUEUE {
+	SPTR  tail;
+	SPTR  head;
+public:
+	SLFQUEUE() {
+		head.ptr = tail.ptr = new NODE(0);
+	}
+	~SLFQUEUE() {}
 
 	void Init() {
-		/*싱글스레드 작동*/
-		NODE* ptr;
-		while (head->next != nullptr) {
-			ptr = head->next;
-			head->next = head->next->next;
+		NODE *ptr;
+		while (head.ptr->next != nullptr) {
+			ptr = head.ptr->next;
+			head.ptr->next = head.ptr->next->next;
 			delete ptr;
 		}
 		tail = head;
 	}
 
+	bool CAS(NODE * volatile * addr, NODE *old_node, NODE *new_node) {
+		return atomic_compare_exchange_strong(reinterpret_cast<volatile atomic_int *>(addr),
+			reinterpret_cast<int *>(&old_node),
+			reinterpret_cast<int>(new_node));
+	}
+
+	bool STAMP_CAS(SPTR* addr, NODE* old_node, int old_stamp, NODE* new_node) {
+		SPTR old_ptr{ old_node,old_stamp };
+		SPTR new_ptr{ new_node,old_stamp + 1 };
+
+		return atomic_compare_exchange_strong(reinterpret_cast<atomic_llong *>(addr),
+			reinterpret_cast<long long*>(&old_ptr),
+			*(reinterpret_cast<long long*>(&new_ptr)));
+	}
 
 	void Enq(int key) {
-		NODE* e = new NODE(key);
+		NODE *e = new NODE(key);
 		while (true) {
-			NODE* last = tail;
-			NODE* next = last->next;
-			if (last != tail)continue;
-			if (next == nullptr) {
-				if (CAS(&(last->next), nullptr, e)) {
-					CAS(&tail, last, e);
-					return;
+			SPTR last = tail;
+			NODE *next = last.ptr->next;
+			if (last.ptr != tail.ptr) continue;
+
+			if (next != nullptr) {
+				//CAS(&tail, last, next);
+				STAMP_CAS(&tail, last.ptr, last.stamp, next);
+				continue;
+
+			}
+			else {
+			if (false == CAS(&last.ptr->next, nullptr, e)) continue;
+			//CAS(&tail, last, e);
+			STAMP_CAS(&tail, last.ptr, last.stamp, e);
+			return; //break;
+			}
+		}
+	}
+
+	int Deq(){
+		while (true) {
+			SPTR first = head;
+			SPTR last = tail;
+			NODE *next = first.ptr->next;   //next는 ABA문제랑 연관이 없다 왜? 로컬변수니까  next값을 CAS넣지도 않는다.
+			NODE *lastnext = last.ptr->next;
+			if (first.ptr != head.ptr) continue;
+			if (last.ptr == first.ptr) {
+				if (lastnext == nullptr) {
+				//	cout << "EMPTY!!!\n";
+				//	this_thread::sleep_for(1ms);
+					return -1;
 				}
 				else
-					CAS(&tail, last, next);
+				{
+					STAMP_CAS(&tail, last.ptr, last.stamp, lastnext);
+					continue;
+				}
 			}
+
+			if (nullptr == next) continue;
+			int result = next->key;
+			if (false == STAMP_CAS(&head, first.ptr, first.stamp, next)) continue;
+			
+			first.ptr->next = nullptr;
+			delete first.ptr;
+			return result;
 		}
 	}
 
-	int Deq() {
-		while (true) {
-			NODE* first = head;
-			NODE* last = tail;
-			NODE* next = first->next;
-			if (first != head)continue;
-			if (first == last) {
-				if (next == nullptr)return -1;
-				CAS(&tail, last, next);
-				continue;
-			}
-			int value = next->key;
-			if (CAS(&head, first, next) == false)continue;
-			delete first;
-			return value;
-		}
-	}
-
-	void display20() {
-		/*싱글쓰레드에서 작동*/
+	void display20()
+	{
 		int c = 20;
-		NODE* p = head->next;
-		while (p != nullptr) {
-			std::cout << p->key << ", ";
+		NODE *p = head.ptr->next;
+		while (p != nullptr)
+		{
+			cout << p->key << ", ";
 			p = p->next;
 			c--;
-			if (c == 0)break;
+			if (c == 0) break;
 		}
-		std::cout << std::endl;
+		cout << endl;
 	}
 };
 
-Cqueue my_queue;
 
-void ThreadFunc(int num_thread) {
-	int key;
-
-	for (int i = 0; i < NUM_TEXT / num_thread; ++i) {
-		if ((rand() % 2 == 0) || i < 10000 / num_thread) {
+SLFQUEUE my_queue;
+void ThreadFunc(int num_thread){
+	for (int i = 0; i < NUM_TEST / num_thread; i++) {
+		if ((rand() % 2) || i < 100 / num_thread) {
 			my_queue.Enq(i);
 		}
 		else {
-			my_queue.Deq();
+			int key = my_queue.Deq();
 		}
 	}
 }
 
-int main() {
-	for (int num_thread = 1; num_thread <= 16; num_thread *= 2) {
-		std::vector<std::thread> threads;
-		my_queue.Init();
-		auto start_time = high_resolution_clock::now();
-		for (int i = 0; i < num_thread; ++i) { threads.emplace_back(ThreadFunc, num_thread); }
-		for (auto &th : threads)th.join();
-		auto end_time = high_resolution_clock::now();
-		threads.clear();
 
-		auto exec_time = end_time - start_time;
-		int exec_ms = std::chrono::duration_cast<std::chrono::milliseconds>(exec_time).count();
-		std::cout << "Threads[" << num_thread << "]" << " Exec_time" << exec_ms << "ms" << std::endl;
+int main() {
+	for (int num_thread = 1; num_thread <= 16; num_thread *= 2){
+		my_queue.Init();
+		vector<thread> threads;
+		auto s = high_resolution_clock::now();
+
+		for (int i = 0; i < num_thread; ++i)
+			threads.emplace_back(ThreadFunc, num_thread);
+
+		for (auto& th : threads) th.join();
+		threads.clear();
+		auto d = high_resolution_clock::now() - s;
+
 		my_queue.display20();
+		//my_queue.recycle_freelist();
+		cout << num_thread << "Threads,  ";
+		cout << ",  Duration : " << duration_cast<milliseconds>(d).count() << " msecs.\n\n";
+
 
 	}
 	system("pause");
 }
+
